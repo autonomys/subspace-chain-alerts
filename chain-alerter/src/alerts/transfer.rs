899f6a10097d8fd9fb66e347089f08025a620edf
teor <teor@riseup.net>
2025-09-23 12:12:20 +0200

    Reduce conditional nesting

--- chain-alerter/src/alerts/transfer.rs
+++ chain-alerter/src/alerts/transfer.rs
@@ -14,67 +14,69 @@ pub async fn check_balance_extrinsic(
     extrinsic_info: &ExtrinsicInfo,
     block_info: &BlockInfo,
 ) -> anyhow::Result<()> {
-    if extrinsic_info.pallet == "Balances" {
-        // "force*" calls and large balance changes are alerts.
+    if extrinsic_info.pallet != "Balances" {
+        return Ok(());
+    }
 
-        // subxt knows these field names, so we can search for the transfer value by
-        // name.
-        // TODO:
-        // - track the total of recent transfers, so the threshold can't be bypassed by splitting
-        //   the transfer into multiple calls
-        // - split this field search into a function which takes a field name, and another function
-        //   which does the numeric conversion and range check
-        let transfer_value: Option<Balance> = if let Composite::Named(named_fields) =
-            &extrinsic_info.fields
-            && let Some((_, transfer_value)) = named_fields
-                .iter()
-                .find(|(name, _)| ["value", "amount", "new_free", "delta"].contains(&name.as_str()))
-        {
-            transfer_value.as_u128()
-        } else {
-            None
-        };
+    // "force*" calls and large balance changes are alerts.
 
-        debug!(?mode, "transfer_value: {:?}", transfer_value);
+    // subxt knows these field names, so we can search for the transfer value by
+    // name.
+    // TODO:
+    // - track the total of recent transfers, so the threshold can't be bypassed by splitting the
+    //   transfer into multiple calls
+    // - split this field search into a function which takes a field name, and another function
+    //   which does the numeric conversion and range check
+    let transfer_value: Option<Balance> = if let Composite::Named(named_fields) =
+        &extrinsic_info.fields
+        && let Some((_, transfer_value)) = named_fields
+            .iter()
+            .find(|(name, _)| ["value", "amount", "new_free", "delta"].contains(&name.as_str()))
+    {
+        transfer_value.as_u128()
+    } else {
+        None
+    };
 
-        // TODO:
-        // - test force alerts by checking a historic block with that call
-        // - do we want to track burn calls? <https://autonomys.subscan.io/extrinsic/137324-31>
-        if extrinsic_info.call.starts_with("force") {
-            alert_tx
-                .send(Alert::new(
-                    AlertKind::ForceBalanceTransfer {
-                        extrinsic_info: extrinsic_info.clone(),
-                        transfer_value,
-                    },
-                    *block_info,
-                    mode,
-                ))
-                .await?;
-        } else if let Some(transfer_value) = transfer_value
-            && transfer_value >= MIN_BALANCE_CHANGE
-        {
-            alert_tx
-                .send(Alert::new(
-                    AlertKind::LargeBalanceTransfer {
-                        extrinsic_info: extrinsic_info.clone(),
-                        transfer_value,
-                    },
-                    *block_info,
-                    mode,
-                ))
-                .await?;
-        } else if transfer_value.is_none()
-            && !["transfer_all", "upgrade_accounts"].contains(&extrinsic_info.call.as_str())
-        {
-            // Every other Balances extrinsic should have an amount.
-            // TODO: check transfer_all by accessing account storage to get the value
-            warn!(
-                ?mode,
-                ?extrinsic_info,
-                "Balance: extrinsic amount unavailable in block",
-            );
-        }
+    debug!(?mode, "transfer_value: {:?}", transfer_value);
+
+    // TODO:
+    // - test force alerts by checking a historic block with that call
+    // - do we want to track burn calls? <https://autonomys.subscan.io/extrinsic/137324-31>
+    if extrinsic_info.call.starts_with("force") {
+        alert_tx
+            .send(Alert::new(
+                AlertKind::ForceBalanceTransfer {
+                    extrinsic_info: extrinsic_info.clone(),
+                    transfer_value,
+                },
+                *block_info,
+                mode,
+            ))
+            .await?;
+    } else if let Some(transfer_value) = transfer_value
+        && transfer_value >= MIN_BALANCE_CHANGE
+    {
+        alert_tx
+            .send(Alert::new(
+                AlertKind::LargeBalanceTransfer {
+                    extrinsic_info: extrinsic_info.clone(),
+                    transfer_value,
+                },
+                *block_info,
+                mode,
+            ))
+            .await?;
+    } else if transfer_value.is_none()
+        && !["transfer_all", "upgrade_accounts"].contains(&extrinsic_info.call.as_str())
+    {
+        // Every other Balances extrinsic should have an amount.
+        // TODO: check transfer_all by accessing account storage to get the value
+        warn!(
+            ?mode,
+            ?extrinsic_info,
+            "Balance: extrinsic amount unavailable in block",
+        );
     }
 
     Ok(())
